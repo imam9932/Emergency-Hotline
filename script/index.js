85b888eb557f984fb1a4f805ed3c15a14cc6fb51
imam9932 <imamhossan9932@gmail.com>
2025-08-27 20:26:55 +0600

    added update items

---
+++ script/index.js
@@ -0,0 +1,59 @@
+// function for heart clicking
+
+let number=1
+function clickAndIncreaseHeart(id){
+
+  const heartIncreaseElement=document.getElementById('heart-increasing-para').innerText
+   
+ let heartIncreaseElements=number++;
+document.getElementById('heart-increasing-para').innerText=heartIncreaseElements
+}
+
+
+
+//  toggle for heart clicking 
+document.getElementById('heart-picture-1').addEventListener('click',function(){
+ 
+clickAndIncreaseHeart('heart-picture-1')
+})
+
+document.getElementById('heart-picture-2').addEventListener('click',function(){
+ 
+clickAndIncreaseHeart('heart-picture-2')
+})
+
+document.getElementById('heart-picture-3').addEventListener('click',function(){
+ 
+clickAndIncreaseHeart('heart-picture-3')
+})
+
+document.getElementById('heart-picture-4').addEventListener('click',function(){
+ 
+clickAndIncreaseHeart('heart-picture-4')
+})
+
+document.getElementById('heart-picture-5').addEventListener('click',function(){
+ 
+clickAndIncreaseHeart('heart-picture-5')
+})
+
+document.getElementById('heart-picture-6').addEventListener('click',function(){
+ 
+clickAndIncreaseHeart('heart-picture-6')
+})
+
+document.getElementById('heart-picture-7').addEventListener('click',function(){
+ 
+clickAndIncreaseHeart('heart-picture-7')
+})
+
+document.getElementById('heart-picture-8').addEventListener('click',function(){
+ 
+clickAndIncreaseHeart('heart-picture-8')
+})
+
+document.getElementById('heart-picture-9').addEventListener('click',function(){
+ 
+clickAndIncreaseHeart('heart-picture-9')
+})
+ 
